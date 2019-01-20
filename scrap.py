973080f482aefd4fbbace565d92ddda62629bf3a
Mohamed Jinas <j@live.mv>
2019-01-21 03:16:21 +0500

    modified sequence handling

--- scrap.py
+++ scrap.py
@@ -11,21 +11,21 @@ def scapp():
     for i in range(50000, 58679):
         num = i
     # url that its pulling
-    response = requests.get('https://avas.mv/' + str(num))
+        response = requests.get('https://avas.mv/' + str(num))
     # parsing the html
-    soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
     # pulling the main class
-    posts = soup.find_all(class_='rtl container mx-auto mb-7 mt-8 px-4 md:px-0')
+        posts = soup.find_all(class_='rtl container mx-auto mb-7 mt-8 px-4 md:px-0')
     # looping it to get individual elements
-    for post in posts:
+        for post in posts:
         # grabbing the header
-        title = post.find('h1').get_text().replace('\n', '')
+          title = post.find('h1').get_text().replace('\n', '')
         # grabbing the link
-        link = post.find('a')['href']
+          link = post.find('a')['href']
         # grabbing the date
-        date = post.find(class_='ltr text-sm text-grey-dark pl-2').get_text().replace('\n', '')
+          date = post.find(class_='ltr text-sm text-grey-dark pl-2').get_text().replace('\n', '')
         # output
-        print(title, link, date)
+          print(title, link, date)
 
         # Writing to a file
         file = open('testfile.txt', 'a+', encoding="utf-8")
